1b562fcb9abad951a5efc3e947ec4c5819b3f69f
Paul Iannazzo <somethingitalian@gmail.com>
2013-12-23 12:40:47 +0800

    removed all references to 'this'. returning object of 2 functions only. helper functions are now nested (no duplicate functions needed) helper constants ALPHA and PADCHAR are duplicated in the 2 returned functions.

--- angular-base64.js
+++ angular-base64.js
@@ -1,5 +1,3 @@
-'use strict';
-
 /*
  * Encapsulation of Nick Galbreath's base64.js library for AngularJS
  * Original notice included below
@@ -54,103 +52,106 @@
  */
 
 angular.module('base64', []).constant('$base64', {
-    PADCHAR: '=',
-    ALPHA: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/',
-    getbyte64: function(s,i) {
-        var idx = this.ALPHA.indexOf(s.charAt(i));
-        if (idx == -1) {
-            throw "Cannot decode base64";
-        }
-        return idx;
-    },
-    decode: function(s) {
-        // convert to string
-        s = "" + s;
-        var getbyte64 = this.getbyte64;
-        var pads, i, b10;
-        var imax = s.length
-        if (imax == 0) {
-            return s;
-        }
-
-        if (imax % 4 != 0) {
-            throw "Cannot decode base64";
-        }
-
-        pads = 0
-        if (s.charAt(imax -1) == this.PADCHAR) {
-            pads = 1;
-            if (s.charAt(imax -2) == this.PADCHAR) {
-                pads = 2;
-            }
-            // either way, we want to ignore this last block
-            imax -= 4;
-        }
-
-        var x = [];
-        for (i = 0; i < imax; i += 4) {
-            b10 = (getbyte64(s,i) << 18) | (getbyte64(s,i+1) << 12) |
-                (getbyte64(s,i+2) << 6) | getbyte64(s,i+3);
-            x.push(String.fromCharCode(b10 >> 16, (b10 >> 8) & 0xff, b10 & 0xff));
-        }
-
-        switch (pads) {
-            case 1:
-                b10 = (getbyte64(s,i) << 18) | (getbyte64(s,i+1) << 12) | (getbyte64(s,i+2) << 6)
-                x.push(String.fromCharCode(b10 >> 16, (b10 >> 8) & 0xff));
-                break;
-            case 2:
-                b10 = (getbyte64(s,i) << 18) | (getbyte64(s,i+1) << 12);
-                x.push(String.fromCharCode(b10 >> 16));
-                break;
-        }
-        return x.join('');
-    },
-    getbyte: function(s,i) {
-        var x = s.charCodeAt(i);
-        if (x > 255) {
-            throw "INVALID_CHARACTER_ERR: DOM Exception 5";
-        }
-        return x;
-    },
-    encode: function(s) {
-        if (arguments.length != 1) {
-            throw "SyntaxError: Not enough arguments";
-        }
-        var padchar = this.PADCHAR;
-        var alpha   = this.ALPHA;
-        var getbyte = this.getbyte;
-
-        var i, b10;
-        var x = [];
-
-        // convert to string
-        s = "" + s;
-
-        var imax = s.length - s.length % 3;
-
-        if (s.length == 0) {
-            return s;
-        }
-        for (i = 0; i < imax; i += 3) {
-            b10 = (getbyte(s,i) << 16) | (getbyte(s,i+1) << 8) | getbyte(s,i+2);
-            x.push(alpha.charAt(b10 >> 18));
-            x.push(alpha.charAt((b10 >> 12) & 0x3F));
-            x.push(alpha.charAt((b10 >> 6) & 0x3f));
-            x.push(alpha.charAt(b10 & 0x3f));
-        }
-        switch (s.length - imax) {
-            case 1:
-                b10 = getbyte(s,i) << 16;
-                x.push(alpha.charAt(b10 >> 18) + alpha.charAt((b10 >> 12) & 0x3F) +
-                    padchar + padchar);
-                break;
-            case 2:
-                b10 = (getbyte(s,i) << 16) | (getbyte(s,i+1) << 8);
-                x.push(alpha.charAt(b10 >> 18) + alpha.charAt((b10 >> 12) & 0x3F) +
-                    alpha.charAt((b10 >> 6) & 0x3f) + padchar);
-                break;
-        }
-        return x.join('');
+  decode : function (s) {
+    'use strict';
+    var PADCHAR = '=';
+    var ALPHA = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+    function getbyte64(s,i) {
+      var idx = ALPHA.indexOf(s.charAt(i));
+      if (idx == -1) {
+        throw "Cannot decode base64";
+      }
+      return idx;
+    }; 
+    // convert to string
+    s = "" + s;
+    var pads, i, b10;
+    var imax = s.length
+    if (imax == 0) {
+      return s;
     }
-});
+
+    if (imax % 4 != 0) {
+      throw "Cannot decode base64";
+    }
+
+    pads = 0
+    if (s.charAt(imax -1) == PADCHAR) {
+      pads = 1;
+      if (s.charAt(imax -2) == PADCHAR) {
+        pads = 2;
+      }
+      // either way, we want to ignore this last block
+      imax -= 4;
+    }
+
+    var x = [];
+    for (i = 0; i < imax; i += 4) {
+      b10 = (getbyte64(s,i) << 18) | (getbyte64(s,i+1) << 12) |
+        (getbyte64(s,i+2) << 6) | getbyte64(s,i+3);
+      x.push(String.fromCharCode(b10 >> 16, (b10 >> 8) & 0xff, b10 & 0xff));
+    }
+
+    switch (pads) {
+    case 1:
+      b10 = (getbyte64(s,i) << 18) | (getbyte64(s,i+1) << 12) | (getbyte64(s,i+2) << 6)
+      x.push(String.fromCharCode(b10 >> 16, (b10 >> 8) & 0xff));
+      break;
+    case 2:
+      b10 = (getbyte64(s,i) << 18) | (getbyte64(s,i+1) << 12);
+      x.push(String.fromCharCode(b10 >> 16));
+      break;
+    }
+    return x.join('');
+  },
+
+  encode : function (s) {
+    'use strict';
+    var PADCHAR = '=';
+    var ALPHA = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+    function getbyte(s,i) {
+      var x = s.charCodeAt(i);
+      if (x > 255) {
+        throw "INVALID_CHARACTER_ERR: DOM Exception 5";
+      }
+      return x;
+    };
+    if (arguments.length != 1) {
+      throw "SyntaxError: Not enough arguments";
+    }
+    var padchar = PADCHAR;
+    var alpha   = ALPHA;
+
+    var i, b10;
+    var x = [];
+
+    // convert to string
+    s = "" + s;
+
+    var imax = s.length - s.length % 3;
+
+    if (s.length == 0) {
+      return s;
+    }
+    for (i = 0; i < imax; i += 3) {
+      b10 = (getbyte(s,i) << 16) | (getbyte(s,i+1) << 8) | getbyte(s,i+2);
+      x.push(alpha.charAt(b10 >> 18));
+      x.push(alpha.charAt((b10 >> 12) & 0x3F));
+      x.push(alpha.charAt((b10 >> 6) & 0x3f));
+      x.push(alpha.charAt(b10 & 0x3f));
+    }
+    switch (s.length - imax) {
+    case 1:
+      b10 = getbyte(s,i) << 16;
+      x.push(alpha.charAt(b10 >> 18) + alpha.charAt((b10 >> 12) & 0x3F) +
+             padchar + padchar);
+      break;
+    case 2:
+      b10 = (getbyte(s,i) << 16) | (getbyte(s,i+1) << 8);
+      x.push(alpha.charAt(b10 >> 18) + alpha.charAt((b10 >> 12) & 0x3F) +
+             alpha.charAt((b10 >> 6) & 0x3f) + padchar);
+      break;
+    }
+    return x.join('');
+  }
+})
